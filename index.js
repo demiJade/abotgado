0e0579fcc89673e4675bd4cdce67810802a23348
Judy Sy <sy_judy@yahoo.com>
2017-02-18 18:10:33 +0800

    test attachment

--- index.js
+++ index.js
@@ -105,45 +105,13 @@ function sendAttachment(recipientId) {
 		}, 
 		message: {
 	      attachment: {
-	        type: "template",
-	        payload: {
-	          template_type: "generic",
-	          elements: [{
-	            title: "rift",
-	            subtitle: "Next-generation virtual reality",
-	            item_url: "https://www.oculus.com/en-us/rift/",               
-	            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-	            buttons: [{
-	              type: "web_url",
-	              url: "https://www.oculus.com/en-us/rift/",
-	              title: "Open Web URL"
-	            }, {
-	              type: "postback",
-	              title: "Call Postback",
-	              payload: "Payload for first bubble",
-	            }],
-	          }, {
-	            title: "touch",
-	            subtitle: "Your Hands, Now in VR",
-	            item_url: "https://www.oculus.com/en-us/touch/",               
-	            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-	            buttons: [{
-	              type: "web_url",
-	              url: "https://www.oculus.com/en-us/touch/",
-	              title: "Open Web URL"
-	            }, {
-	              type: "postback",
-	              title: "Call Postback",
-	              payload: "Payload for second bubble",
-	            }]
-	          }, {
+	        
 	          	type:"file",
 		        payload:{
 		          url:"http://www.lawphil.net/judjuris/juri2017/jan2017/pdf/gr_187448_2017.pdf"
 		        }
-	          }]
-	        }
-	      }
+	          }
+
 	    }
 	}
 	callSendAPI(messageData);
