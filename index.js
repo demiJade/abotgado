ef113ce144149bbbb386fb3f2af4d5842d414303
Judy Sy <sy_judy@yahoo.com>
2017-02-18 15:34:53 +0800

    changed webhook to /

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/webhook/', function (req, res) {
 })
 
 // to post data
-app.post('/webhook/', function (req, res) {
+app.post('/', function (req, res) {
 	let messaging_events = req.body.entry[0].messaging
 	for (let i = 0; i < messaging_events.length; i++) {
 		let event = req.body.entry[0].messaging[i]
