b9e8bc266dc1e1279a1ac77c0aeddd9288ef88bd
Judy Sy <sy_judy@yahoo.com>
2017-02-18 22:59:18 +0800

    try generic

--- index.js
+++ index.js
@@ -116,21 +116,24 @@ function receivedMessage(event) {
   var messageText = message.text;
   var messageAttachments = message.attachments;
   
-  if (messageText) {
+  if (messageText == 'rift') {
 
     // If we receive a text message, check to see if it matches a keyword
     // and send back the example. Otherwise, just echo the text we received.
-    switch (messageText) {
-      case 'generic':
+    // switch (messageText) {
+    //   case 'generic':
         sendGenericMessage(senderID);
-        break;
+      //   break;
 
-      default:
+      // default:
       	// sendAttachment(senderID);
       	// sendGenericMessage(senderID);
-        sendTextMessage(senderID, "Got it!");
-    }
-  } else if (messageAttachments) {
+        
+    // }
+  } else if (messageText){
+  	sendTextMessage(senderID, "Got it!");
+  }
+  else if (messageAttachments) {
     sendTextMessage(senderID, "Message with attachment received");
   }
 }
@@ -226,6 +229,54 @@ function sendGenericMessage(recipientId) {
               type: "postback",
               title: "Call Postback",
               payload: "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  };  
+
+  callSendAPI(messageData);
+}
+
+function sendPostbackMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",               
+            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",               
+            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+
             }]
           }]
         }
